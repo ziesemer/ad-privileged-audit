cae1b0f9fd9b3868d15e8fe137076961484835f0
Mark A. Ziesemer <online@mark.ziesemer.com>
2021-04-06 21:51:16 -0500

    Refactor GridView, file writes to common Out-Reports function.

--- AD-Privileged-Audit.ps1
+++ AD-Privileged-Audit.ps1
@@ -1,7 +1,7 @@
 #Requires -Version 5.1
 #Requires -Modules ActiveDirectory
 
-# Mark A. Ziesemer, www.ziesemer.com - 2020-08-27, 2021-03-31
+# Mark A. Ziesemer, www.ziesemer.com - 2020-08-27, 2021-04-06
 
 Param(
 	# Technically, most of this works without elevation - but certain AD queries will not work properly without,
@@ -59,7 +59,7 @@ function Write-Log{
 }
 
 function Convert-Timestamps{
-	[cmdletbinding()]
+	[CmdletBinding()]
 	param(
 		[Parameter(Mandatory, ValueFromPipeline)]
 		[PSCustomObject]$row,
@@ -82,25 +82,56 @@ function Convert-Timestamps{
 	}
 }
 
+function Out-Reports{
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[object[]]$results,
+		[Parameter(Mandatory)]
+		$ctx,
+		[Parameter(Mandatory)]
+		[string]$name,
+		[string]$title
+	)
+	Begin{
+		Write-Log "Processing $title ($name)..."
+	}
+	End{
+		$ctx.reports.$name = $results
+		$results | Export-Csv -NoTypeInformation -Path $ExecutionContext.InvokeCommand.ExpandString($ctx.filePattern)
+		$caption = "  $title ($name): $($results.Count)"
+		Write-Log $caption
+		if($results -and $interactive){
+			$results | Out-GridView -Title $caption
+		}
+	}
+}
+
 function Invoke-Reports(){
-	$out = [ordered]@{}
+	$out = [ordered]@{
+		params = [ordered]@{}
+		reports = [ordered]@{}
+		filePattern = $null
+	}
 
 	if(!$reportsFolder){
 		$desktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
 		$reportsFolder = "$desktopPath\AD-Reports"
 	}
-	$out.reportsFolder = $reportsFolder
+	$out.params.reportsFolder = $reportsFolder
 	Write-Log "`$reportsFolder: $($reportsFolder)"
 	[void](New-Item -ItemType Directory -Path $reportsFolder -Force)
 
-	$now = Get-Date
-	$filterDate = $out.filterDate = $now.AddDays(-90)
+	$now = $out.params.now = Get-Date
+	Write-Log "`$now: $now"
+	$filterDate = $out.params.filterDate = $now.AddDays(-90)
 	Write-Log "`$filterDate: $filterDate"
-	$filterDatePassword = $out.filterDatePasword = $now.AddDays(-365)
+	$filterDatePassword = $out.params.filterDatePassword = $now.AddDays(-365)
 	Write-Log "`$filterDatePassword: $filterDatePassword"
 
-	$csvDateExt = '-' + $(Get-Date -Format 'yyyy-MM-dd') + '.csv'
-	Write-Log "`$csvDateExt: $csvDateExt"
+	$filePattern = $out.filePattern = Join-Path $reportsFolder `
+		('$($name)-' + $(Get-Date -Date $now -Format 'yyyy-MM-dd') + '.csv')
+	Write-Log "`$filePattern: $filePattern"
 
 	$currentThread = [System.Threading.Thread]::CurrentThread
 	$culture = [CultureInfo]::InvariantCulture.Clone()
@@ -130,9 +161,8 @@ function Invoke-Reports(){
 		}
 	}
 
-	Write-Log 'Processing AD Privileged Groups...'
+	# Privileged AD Group Members...
 
-	$privGroupMembers = $out.privGroupMembers = @(
 	# - https://docs.microsoft.com/en-us/windows-server/identity/ad-ds/plan/security-best-practices/appendix-b--privileged-accounts-and-groups-in-active-directory
 	('Domain Admins', 'Administrators', 'Schema Admins', 'Enterprise Admins',
 			'Account Operators', 'Backup Operators', 'Server Operators',
@@ -176,84 +206,57 @@ function Invoke-Reports(){
 
 			[PSCustomObject]$x
 		}
-	} | Convert-Timestamps | Select-Object -Property @(,'Group' + $commonAdPropsOut) )
-
-	$privGroupMembers | Export-Csv -NoTypeInformation -Path (Join-Path $reportsFolder ('GroupMembers' + $csvDateExt))
-	if($interactive){
-		$privGroupMembers | Out-GridView -Title "Privileged AD Group Members ($($privGroupMembers.Count))"
-	}
+	} | Convert-Timestamps `
+		| Select-Object -Property @(,'Group' + $commonAdPropsOut) `
+		| Out-Reports -ctx $out -name 'privGroupMembers' -title 'Privileged AD Group Members'
 
 	# Users that haven't logged-in within # days...
 
-	Write-Log 'Processing stale users...'
-
-	$staleUsers = $out.staleUsers = @(Get-ADUser `
+	Get-ADUser `
 			-Filter {
 				Enabled -eq $true -and (lastLogonTimestamp -lt $filterDate -or lastLogonTimestamp -notlike '*')
 			} `
 			-Properties $commonAdPropsIn `
 		| Convert-Timestamps `
 		| Sort-Object -Property lastLogonTimestamp `
-		| Select-Object -Property $commonAdPropsOut)
-	$staleUsers | Export-Csv -NoTypeInformation -Path (Join-Path $reportsFolder ('staleUsers' + $csvDateExt))
-	Write-Log "Stale users: $($staleUsers.Count)"
-	if($staleUsers -and $interactive){
-		$staleUsers | Out-GridView -Title "Stale Users ($($staleUsers.Count))"
-	}
+		| Select-Object -Property $commonAdPropsOut `
+		| Out-Reports -ctx $out -name 'staleUsers' -title 'Stale Users'
 
 	# Users with passwords older than # days...
 
-	Write-Log 'Processing stale passwords...'
-
-	$stalePasswords = $out.stalePasswords = @(Get-ADUser `
+	Get-ADUser `
 			-Filter {
 				Enabled -eq $true -and (PasswordLastSet -lt $filterDatePassword)
 			} `
 			-Properties $commonAdPropsIn `
 		| Convert-Timestamps `
 		| Sort-Object -Property PasswordLastSet `
-		| Select-Object -Property $commonAdPropsOut)
-	$stalePasswords | Export-Csv -NoTypeInformation -Path (Join-Path $reportsFolder ('stalePasswords' + $csvDateExt))
-	Write-Log "Stale Passwords: $($stalePasswords.Count)"
-	if($stalePasswords -and $interactive){
-		$stalePasswords | Out-GridView -Title "Stale Passwords ($($stalePasswords.Count))"
-	}
+		| Select-Object -Property $commonAdPropsOut `
+		| Out-Reports -ctx $out -name 'stalePasswords' -title 'Stale Passwords'
 
 	# Computers that haven't logged-in within # days...
 
-	Write-Log 'Processing stale computers...'
-
-	$staleComps = $out.staleComputers = @(Get-ADComputer `
+	Get-ADComputer `
 			-Filter {
 				Enabled -eq $true -and (lastLogonTimestamp -lt $filterDate -or lastLogonTimestamp -notlike '*')
 			} `
 			-Properties $commonAdPropsIn `
 		| Convert-Timestamps `
 		| Sort-Object -Property lastLogonTimestamp `
-		| Select-Object -Property $commonAdPropsOut)
-	$staleComps | Export-Csv -NoTypeInformation -Path (Join-Path $reportsFolder ('staleComps' + $csvDateExt))
-	Write-Log "Stale Computers: $($staleComps.Count)"
-	if($staleComps -and $interactive){
-		$staleComps | Out-GridView -Title "Stale Computers ($($staleComps.Count))"
-	}
+		| Select-Object -Property $commonAdPropsOut `
+		| Out-Reports -ctx $out -name 'staleComps' -title 'Stale Computers'
 
 	# Computers that haven't recently checked-in to LAPS...
 
-	Write-Log 'Processing LAPS...'
-
 	$admPwdAttr = Get-ADObject -SearchBase (Get-ADRootDSE).SchemaNamingContext -Filter {name -eq 'ms-Mcs-AdmPwd'}
 	if($admPwdAttr){
-		$noLAPS = $out.noLAPS = @(Get-ADComputer -Filter {
+		Get-ADComputer -Filter {
 					Enabled -eq $true -and (ms-Mcs-AdmPwd -notlike '*' -or ms-Mcs-AdmPwdExpirationTime -lt $filterDate)
 				} `
 				-Properties ($commonAdPropsIn + 'ms-Mcs-AdmPwdExpirationTime') `
 			| Convert-Timestamps -dateProps 'lastLogonTimestamp', 'ms-Mcs-AdmPwdExpirationTime' `
-			| Select-Object -Property (@('ms-Mcs-AdmPwdExpirationTimeDate', 'ms-Mcs-AdmPwdExpirationTime') + $commonAdPropsOut))
-		$noLAPS | Export-Csv -NoTypeInformation -Path (Join-Path $reportsFolder ('noLAPS' + $csvDateExt))
-		Write-Log "Missing LAPS: $($noLAPS.Count)"
-		if($noLAPS -and $interactive){
-			$noLAPS | Out-GridView -Title "Computers without LAPS ($($noLAPS.Count))"
-		}
+			| Select-Object -Property (@('ms-Mcs-AdmPwdExpirationTimeDate', 'ms-Mcs-AdmPwdExpirationTime') + $commonAdPropsOut) `
+			| Out-Reports -ctx $out -name 'noLAPS' -title 'Computers without LAPS'
 	}else{
 		Write-Log 'LAPS is not deployed!  (ms-Mcs-AdmPwd attribute does not exist.)' -Severity WARN
 	}
